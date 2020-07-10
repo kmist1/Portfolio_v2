631b906b1d1719eebd56af216bd6c2301bf3a444
Krunal <kmist1@unh.newhaven.edu>
2020-07-10 02:05:05 -0400

    configured AWS-sdk

--- js/Server.js
+++ js/Server.js
@@ -4,5 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var aws_sdk_1 = __importDefault(require("aws-sdk"));
 var app = express_1.default();
-app.listen(5000, function () { return console.log("server is up!"); });
+var bodyParser = require('body-parser');
+// Set region for AWS SDKs
+aws_sdk_1.default.config.region = process.env.REGION;
+app.set('view engine', 'pug');
+app.set('build', __dirname + '/index.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Krunal Mistry'
+    });
+    res.status(200).end();
+});
+var port = process.env.PORT || 3000;
+app.listen(port, function () { return console.log("server is up!"); });
--- src/Server.ts
+++ src/Server.ts
@@ -1,7 +1,28 @@
 import Express, { Application, Request, Response } from 'express';
 
+import AWS from 'aws-sdk';
+
 const app: Application = Express();
+const bodyParser = require('body-parser');
+
+
+// Set region for AWS SDKs
+AWS.config.region = process.env.REGION
+
+app.set('view engine', 'pug')
+app.set('build', __dirname + '/index.html')
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Krunal Mistry'
+    })
+    res.status(200).end();
+})
+
+var port = process.env.PORT || 3000
 
 
 
-app.listen(5000, ()=>console.log("server is up!"));
+
+app.listen(port, ()=>console.log("server is up!"));
