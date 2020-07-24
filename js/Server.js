d8ab5df9c28da7915d713a098dee8b26cd1e4aad
Krunal <kmist1@unh.newhaven.edu>
2020-07-24 01:50:14 -0400

    aws pipleline test

--- js/Server.js
+++ js/Server.js
@@ -4,5 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var app = express_1.default();
-app.listen(5000, function () { return console.log("server is up!"); });
+var path = require('path');
+var http = require('http');
+var server = express_1.default();
+// Specify where the compiled React app lives (copied the files manually from the client build)
+var clientAppDirectory = path.join(__dirname, '../public', 'build');
+server.use(express_1.default.json());
+// Middleware that allows users to get files from a directory on the server
+server.use(express_1.default.static(clientAppDirectory));
+// Any other GET request that doesn't match previous routes should return the website
+server.get('/*', function (request, response) {
+    var indexPath = path.join(clientAppDirectory, 'index.html');
+    return response.sendFile(indexPath);
+});
+var port = process.env.PORT || '4000';
+server.set('port', port);
+server.listen(port, function () { return console.log("React  Running on localhost:" + port); });
--- src/Server.ts
+++ src/Server.ts
@@ -1,7 +1,30 @@
-import Express, { Application, Request, Response } from 'express';
+import Express, {request, response} from 'express'
+const path = require('path');
+const http = require('http');
 
-const app: Application = Express();
+
+const server = Express();
+
+// Specify where the compiled React app lives (copied the files manually from the client build)
+const clientAppDirectory = path.join(__dirname, '../public', 'build');
+
+server.use(Express.json());
+
+// Middleware that allows users to get files from a directory on the server
+server.use(Express.static(clientAppDirectory));
 
 
+// Any other GET request that doesn't match previous routes should return the website
+server.get('/*', (request, response) => {
 
-app.listen(5000, ()=>console.log("server is up!"));
+    const indexPath = path.join(clientAppDirectory, 'index.html');
+
+    return response.sendFile(indexPath);
+});
+
+
+const port = process.env.PORT || '4000';
+server.set('port', port);
+
+
+server.listen(port, () => console.log(`React  Running on localhost:${port}`));
